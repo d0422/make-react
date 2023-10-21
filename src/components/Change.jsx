e6ad3cfdf4baaf05f0ea86e82f9b14e5f52ee2af
d0422 <rlfehd2013@naver.com>
2023-10-21 16:11:41 +0900

    feat: 전체 리렌더하는 코드 추가

---
+++ src/components/Change.jsx
@@ -0,0 +1,4 @@
+import React from "../core/React";
+export const Change = () => {
+  return <h1>바꼈지롱~</h1>;
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import App from './components/App';
 import React from './core/React';
+import { Change } from './components/Change';
 
 const app = document.getElementById('root');
 React.render(App(), app);
+setTimeout(() => {
+  app.innerHTML = '';
+  React.render(Change(), app);
+}, 3000);
