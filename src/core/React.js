e4bd8a4a12fdac45ff0dfbcaca215006d69de844
d0422 <rlfehd2013@naver.com>
2023-10-22 21:25:35 +0900

    feat: updateDOM 기능 구현

--- src/core/React.js
+++ src/core/React.js
@@ -1,9 +1,11 @@
 import { createElement } from './createElement';
 import { render } from './render';
+import { updateDOM } from './updateDOM';
 
 const React = {
   createElement,
   render,
+  updateDOM,
 };
 
 export default React;
---
+++ src/core/updateDOM.js
@@ -0,0 +1,64 @@
+import {
+  getDeleteKeysArray,
+  getDiffKeysObject,
+  getNewKeysObject,
+} from '../utils';
+import { makeDOM } from './makeDOM';
+
+const updateAttributes = (target, newProps, oldProps) => {
+  const diffProps = getDiffKeysObject(oldProps, newProps);
+  const addedProps = getNewKeysObject(oldProps, newProps);
+  const deletedProps = getDeleteKeysArray(oldProps, newProps);
+  if (
+    Object.keys(diffProps).length === 0 &&
+    Object.keys(addedProps).length === 0 &&
+    deletedProps.length === 0
+  )
+    return;
+  deletedProps.forEach((key) => {
+    if (key.match(/^on/)) {
+      target[key] = null;
+    }
+    if (key === 'className') {
+      target.removeAttribute('class');
+    }
+    delete target[key];
+    target.removeAttribute(key);
+  });
+
+  Object.keys({ ...diffProps, ...addedProps }).forEach((key) => {
+    target[key] = newProps[key];
+  });
+};
+
+export const updateDOM = (parent, prev, cur, index = 0) => {
+  if (prev && !cur) {
+    if (typeof prev === 'string' || typeof prev === 'number') {
+      return (parent.innerHTML = '');
+    }
+    return prev.ref?.remove();
+  }
+
+  if (!prev && cur) {
+    return parent.appendChild(makeDOM(cur));
+  }
+
+  if (typeof cur === 'string' || typeof cur === 'number') {
+    if (cur === prev) return;
+    return parent.replaceChild(makeDOM(cur), parent.childNodes[index]);
+  }
+
+  if (cur.tagName !== prev.tagName) {
+    return parent.replaceChild(makeDOM(cur), parent.childNodes[index]);
+  }
+
+  if (parent.children[index]) {
+    updateAttributes(parent.children[index], cur.props || {}, prev.props || {});
+    const length = getLongerArrayLength(prev.children, cur.children);
+
+    createArray(length).forEach((i) =>
+      updateDOM(parent.children[index], prev.children[i], cur.children[i], i)
+    );
+  }
+  cur.ref = prev.ref;
+};
