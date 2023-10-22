c2abccd31ecff7ff7979111c8d30fe5aa0d747d7
d0422 <rlfehd2013@naver.com>
2023-10-22 22:30:55 +0900

    chore: 주석 추가

--- src/core/render.js
+++ src/core/render.js
@@ -1,21 +1,23 @@
-let prev;
-let root;
 import App from '../components/App';
 import { makeDOM } from './makeDOM';
 import { updateDOM } from './updateDOM';
+
+let prev; //이전의 virtual DOM
+let root; //최상단 root요소
+
 /**
  * @param {Object} content
  * @param {HTMLElement} container
  */
 export const render = (content, container) => {
-  root = container;
-  prev = content;
+  root = container; //최상단 요소를 최초렌더(mount)시 등록
+  prev = content; // 현재 virtual DOM을 최초 mount컴포넌트로 등록
   const element = makeDOM(content);
   container.appendChild(element);
 };
 
 export const rerender = () => {
   const updateElement = App();
-  updateDOM(root, prev, App());
-  prev = updateElement;
+  updateDOM(root, prev, updateElement);
+  prev = updateElement; //업데이트가 끝나면, 요소 업데이트
 };
