b857f5ac2fb0e79266a5c6f913c1a1ceda99880b
d0422 <rlfehd2013@naver.com>
2023-10-22 23:35:26 +0900

    fix: useState 인덱스 오류 해결

--- src/core/useState.js
+++ src/core/useState.js
@@ -1,18 +1,18 @@
 import { rerender } from './render';
 
-const states = [];
+const states = []; //배열 형태의 states
 let stateIndex = 0;
 
 export const useState = (initialValue) => {
+  const indexBind = stateIndex; //stateIndex가 ++형태로 변경되므로 bind시켜주는 변수
   if (states[stateIndex] === undefined) {
     states.push(initialValue);
     stateIndex++;
   }
 
   const setState = (updateValue) => {
-    states[stateIndex - 1] = updateValue;
+    states[indexBind] = updateValue;
     rerender();
   };
-
-  return [states[stateIndex - 1], setState];
+  return [states[indexBind], setState];
 };
