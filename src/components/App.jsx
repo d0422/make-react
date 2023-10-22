aabce9bca747340563166316aa0204d193101910
d0422 <rlfehd2013@naver.com>
2023-10-22 23:24:50 +0900

    feat: useState를 2회이상쓰는 코드 작성

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,25 @@ import { Header } from './Header';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(10);
   return (
     <div>
       <Header />
+      <div>Counter1</div>
       <span>{count}</span>
       <button
         onclick={() => {
           setCount(count + 1);
+        }}
+      >
+        Click me!
+      </button>
+
+      <div>Counter2</div>
+      <span>{count2}</span>
+      <button
+        onclick={() => {
+          setCount2(count2 + 1);
         }}
       >
         Click me!
